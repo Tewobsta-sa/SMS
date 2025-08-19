98d1f548aca968299b4fa6690bc1e16028dbd6f9
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-19 14:51:50 +0300

    Refactor PageForm and PagesTable classes to enhance schema configuration and add updated_at and deleted_at columns

--- school/app/Filament/Resources/Pages/Schemas/PageForm.php
+++ school/app/Filament/Resources/Pages/Schemas/PageForm.php
@@ -4,13 +4,15 @@ namespace App\Filament\Resources\Pages\Schemas;
 
 use Filament\Schemas\Schema;
 
-class PageForm
-{
-    public static function configure(Schema $schema): Schema
-    {
+class PageForm {
+    public static function configure( Schema $schema ): Schema {
         return $schema
-            ->components([
-                //
-            ]);
+        ->components( [
+            \Filament\Forms\Components\TextInput::make( 'title' )->required()->maxLength( 255 ),
+            \Filament\Forms\Components\TextInput::make( 'slug' )->unique( ignoreRecord: true ),
+            \Filament\Forms\Components\Textarea::make( 'short_description' ),
+            \Filament\Forms\Components\Toggle::make( 'is_active' )->default( true ),
+            \Filament\Forms\Components\TextInput::make( 'display_order' )->numeric()->default( 0 ),
+        ] );
     }
 }
--- school/app/Filament/Resources/Pages/Tables/PagesTable.php
+++ school/app/Filament/Resources/Pages/Tables/PagesTable.php
@@ -10,26 +10,33 @@ use Filament\Actions\RestoreBulkAction;
 use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
 
-class PagesTable
-{
-    public static function configure(Table $table): Table
-    {
+class PagesTable {
+    public static function configure( Table $table ): Table {
         return $table
-            ->columns([
-                //
-            ])
-            ->filters([
-                TrashedFilter::make(),
-            ])
-            ->recordActions([
-                EditAction::make(),
-            ])
-            ->toolbarActions([
-                BulkActionGroup::make([
-                    DeleteBulkAction::make(),
-                    ForceDeleteBulkAction::make(),
-                    RestoreBulkAction::make(),
-                ]),
-            ]);
+        ->columns( [
+            \Filament\Tables\Columns\TextColumn::make( 'title' )->searchable()->sortable(),
+            \Filament\Tables\Columns\TextColumn::make( 'slug' )->sortable(),
+            \Filament\Tables\Columns\TextColumn::make( 'display_order' )->sortable(),
+            \Filament\Tables\Columns\IconColumn::make( 'is_active' )->boolean(),
+            \Filament\Tables\Columns\TextColumn::make( 'created_at' )->dateTime()->sortable()->toggleable(isToggledHiddenByDefault: true),
+            \Filament\Tables\Columns\TextColumn::make( 'updated_at' )->dateTime()->sortable()->toggleable(isToggledHiddenByDefault: true),
+            \Filament\Tables\Columns\TextColumn::make( 'deleted_at' )
+                ->dateTime()
+                ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+        ] )
+        ->filters( [
+            TrashedFilter::make(),
+        ] )
+        ->recordActions( [
+            EditAction::make(),
+        ] )
+        ->toolbarActions( [
+            BulkActionGroup::make( [
+                DeleteBulkAction::make(),
+                ForceDeleteBulkAction::make(),
+                RestoreBulkAction::make(),
+            ] ),
+        ] );
     }
 }
