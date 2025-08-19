369d137c47a92943d0712579dc4daf2fa782ca2e
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-19 12:23:01 +0300

    Remove unnecessary whitespace and clean up index view layout

--- school/resources/views/index.blade.php
+++ school/resources/views/index.blade.php
@@ -1,22 +1,11 @@
 @extends('layouts.app')
 
-
-
-
 <x-heroSection />
 <x-aboutSection />
-
 <x-featuredSection />
-
-
 <x-studentsLifeSection />
-
-
 <x-testimonialsSection />
-
-
 <x-statsSection />
-
 <x-newsSection />
 <x-eventSection />
 
