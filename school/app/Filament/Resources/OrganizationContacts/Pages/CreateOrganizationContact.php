e02511d27a14f0f1a566ac37c3d3eed101966f9e
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-20 14:50:44 +0300

    Add CreateOrganizationContact page for managing organization contacts

---
+++ school/app/Filament/Resources/OrganizationContacts/Pages/CreateOrganizationContact.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\OrganizationContacts\Pages;
+
+use App\Filament\Resources\OrganizationContacts\OrganizationContactResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateOrganizationContact extends CreateRecord
+{
+    protected static string $resource = OrganizationContactResource::class;
+}
