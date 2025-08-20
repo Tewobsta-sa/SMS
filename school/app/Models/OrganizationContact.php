e93391f237586e2865ffb6883dae5b677ac8b7d2
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-20 14:27:01 +0300

    Implement soft deletes and define fillable attributes in OrganizationContact model

--- school/app/Models/OrganizationContact.php
+++ school/app/Models/OrganizationContact.php
@@ -2,9 +2,34 @@
 
 namespace App\Models;
 
+use App\Enums\StatusEnum;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class OrganizationContact extends Model
 {
-    //
+    use SoftDeletes;
+    
+    protected $fillable = [
+        'type',
+        'value',
+        'status'
+    ];
+
+    protected $casts = [
+        'type' => 'string',
+        'status'=> StatusEnum::class,
+
+    ];
+
+  
+    public static function getTypeOptions(): array
+    {
+        return [
+            'phone' => 'Phone',
+            'fax' => 'Fax',
+            'email' => 'Email',
+        ];
+    }
 }
