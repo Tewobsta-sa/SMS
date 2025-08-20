7e019ec55979d5d8fdf27b9549a8b4a94227fdbd
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-20 14:27:12 +0300

    Add fields for type, status, and value to organization_contacts migration

--- school/database/migrations/2025_08_20_112125_create_organization_contacts_table.php
+++ school/database/migrations/2025_08_20_112125_create_organization_contacts_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\StatusEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,7 +14,11 @@ return new class extends Migration
     {
         Schema::create('organization_contacts', function (Blueprint $table) {
             $table->id();
+            $table->enum('type', ['phone', 'fax', 'email']);
+            $table->string('status')->default(StatusEnum::active->value);
+            $table->string('value', 255);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
