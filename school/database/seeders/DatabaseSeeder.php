2f67322b842281e0a011040d82339fe07e6d6c02
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-20 15:09:47 +0300

    Update DatabaseSeeder to include OrganizationSeeder and comment out UserSeeder

--- school/database/seeders/DatabaseSeeder.php
+++ school/database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         $this->call([
-            UserSeeder::class,
-            PageSeeder::class
+            // UserSeeder::class,
+            PageSeeder::class,
+            OrganizationSeeder::class,
 
         ]);
     }
