92c976fc04f2b880acd6a8af5810d6a769df3af2
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-28 15:40:43 +0300

    Implement Schoolclass resource with create, edit, list pages and table configuration

---
+++ app/Filament/Resources/Schoolclasses/Pages/CreateSchoolclass.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\Schoolclasses\Pages;
+
+use App\Filament\Resources\Schoolclasses\SchoolclassResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateSchoolclass extends CreateRecord
+{
+    protected static string $resource = SchoolclassResource::class;
+}
